7d0ace1a62dac28f6af6875882e58b7619df46fc
Uthallan <emillyoadamo@gmail.com>
2024-05-05 22:10:03 -0400

    ff again

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -122,7 +122,7 @@ function ff($name) {
     Get-ChildItem -recurse -filter "*${name}*" -ErrorAction SilentlyContinue | ForEach-Object {
         Write-Output $_.FullName  # Using the FullName property
     }
-}ff
+}
 
 # Network Utilities
 function Get-PubIP { (Invoke-WebRequest http://ifconfig.me/ip).Content }
